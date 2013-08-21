c0009bc17697fb41003ebdf80189f749a4af11d0
David Savage <david.savage@mac.com>
2013-08-21 20:51:55 +1000

    Moved database code to EnronIO. Added UI to ask if data should be reloaded and to allow Cypher queries to be entered and evaluated.

--- src/edu/rmit/EnronIO.java
+++ src/edu/rmit/EnronIO.java
@@ -1,11 +1,18 @@
 package edu.rmit;
 
+import org.neo4j.cypher.CypherException;
+import org.neo4j.cypher.javacompat.ExecutionEngine;
+import org.neo4j.graphdb.*;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+import org.neo4j.graphdb.index.UniqueFactory;
+
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.LinkedList;
+import java.util.Map;
 
 /**
  * Created with IntelliJ IDEA.
@@ -16,14 +23,99 @@ import java.util.LinkedList;
  */
 
 public class EnronIO {
-    public static List<EnronEmail> readFromCSV(String filename) {
-	    LinkedList<EnronEmail> entries = new LinkedList<EnronEmail>();
-	    String line;
+	private String dbPath;
+	private String workingDirectory;
+	private GraphDatabaseService neo4jDB;
+	private ExecutionEngine ex;
 
-	    try {
-		    BufferedReader br = new BufferedReader(new FileReader(filename));
-		    while((line = br.readLine()) != null) entries.add(new EnronEmail(line.split(",")));
-		    br.close();
+	private static enum RelTypes implements RelationshipType {
+		EMAILED
+	}
+
+	public EnronIO(String workingDirectory) {
+		this.workingDirectory = workingDirectory;
+		dbPath = workingDirectory + "/neo4j-db";
+
+		//Instantiate the database server object
+		neo4jDB = new GraphDatabaseFactory().newEmbeddedDatabase(dbPath);
+
+		//Instantiate the Cypher execution engine
+		ex = new ExecutionEngine(neo4jDB);
+	}
+
+
+	public String runCypherQuery(String query) {
+		try {
+			return ex.execute(query).dumpToString();
+		}
+		catch(CypherException e) {
+			System.err.println("Invalid Cypher query: " + query);
+			e.printStackTrace();
+			return "";
+		}
+	}
+
+
+	public void closeDBConnection() {
+		neo4jDB.shutdown();
+	}
+
+
+	public void loadEnronDataSet() {
+		List<EnronEmail> entries = this.readFromCSV("data/mid_from_to_date_len.txt");
+		UniqueFactory<Node> factory;
+
+		Transaction tx = neo4jDB.beginTx();
+		try {
+			//Create a factory object that stores indices in an index set named employees
+			factory = new UniqueFactory.UniqueNodeFactory(neo4jDB, "employees") {
+				protected void initialize(Node created, Map<String, Object> properties) {
+					created.setProperty("id", properties.get("id"));
+				}
+			};
+		}
+		finally {
+			tx.finish();
+		}
+
+		for(EnronEmail email: entries) {
+			addEmail(factory, email);
+		}
+	}
+
+
+	public void addEmail(UniqueFactory<Node> factory, EnronEmail email) {
+		Node fromEmp, toEmp;
+		Relationship rel;
+
+		Transaction tx = neo4jDB.beginTx();
+		try {
+			//Create the from and to nodes if they don't already exist
+			fromEmp = factory.getOrCreate("id", email.to);
+			toEmp = factory.getOrCreate("id", email.from);
+
+			//Create a relationship between the nodes representing the email
+			rel = fromEmp.createRelationshipTo(toEmp, RelTypes.EMAILED);
+			rel.setProperty("date", email.sendDate);
+			rel.setProperty("time", email.sendTime);
+			rel.setProperty("length", email.length);
+
+			tx.success();
+		}
+		finally {
+			tx.finish();
+		}
+	}
+
+
+	private LinkedList<EnronEmail> readFromCSV(String filename) {
+		LinkedList<EnronEmail> entries = new LinkedList<EnronEmail>();
+		String line;
+
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(filename));
+			while((line = br.readLine()) != null) entries.add(new EnronEmail(line.split(",")));
+			br.close();
 		}
 		catch(FileNotFoundException e) {
 			e.printStackTrace();
@@ -32,7 +124,6 @@ public class EnronIO {
 			e.printStackTrace();
 		}
 
-	    return entries;
+		return entries;
 	}
-
 }
--- src/edu/rmit/Main.java
+++ src/edu/rmit/Main.java
@@ -1,85 +1,46 @@
 package edu.rmit;
 
-import org.neo4j.graphdb.factory.GraphDatabaseFactory;
-import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Transaction;
-import org.neo4j.graphdb.Relationship;
-import org.neo4j.graphdb.RelationshipType;
-import org.neo4j.graphdb.Node;
-
-import org.neo4j.cypher.javacompat.ExecutionEngine;
-import org.neo4j.cypher.javacompat.ExecutionResult;
-import org.neo4j.graphdb.index.UniqueFactory;
-
-import java.util.List;
-import java.util.Map;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-	private static final String DB_PATH = "target/neo4j-db";
-
-	private static enum RelTypes implements RelationshipType {
-		EMAILED
-	}
-
     public static void main(String[] args) {
-	    //Instantiate the database server object
-	    GraphDatabaseService graphDB = new GraphDatabaseFactory().newEmbeddedDatabase(DB_PATH);
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	    EnronIO eio = new EnronIO("target");
+	    String line = "";
 
-	    loadEnronDataSet(graphDB);
+        System.out.println("Reload Enron Data Set?");
+	    line = getInputFromPrompt(in).toLowerCase();
+		if(line.equals("yes") || line.equals("y")) {
+			eio.loadEnronDataSet();
+		}
 
-	    ExecutionEngine ex = new ExecutionEngine(graphDB);
-	    String res = ex.execute(
-			    "start n=node(*) return count(n)").dumpToString();
-	    System.out.print(res);
+	    System.out.println("Enter Cypher queries to interact with the database, e.g.");
+	    System.out.println("\"start n=node(*) return count(n)\"\n");
+	    System.out.println("Enter \"quit\" to quit\n");
 
-	    graphDB.shutdown();
+	    while(!line.toLowerCase().equals("quit")) {
+		    line = getInputFromPrompt(in);
+		    if(!line.toLowerCase().equals("quit")) {
+		        String res = eio.runCypherQuery(line);
+		        System.out.println(res);
+		    }
+	    }
+
+	    eio.closeDBConnection();
     }
 
 
-	public static void loadEnronDataSet(GraphDatabaseService graphDB) {
-		List<EnronEmail> entries = EnronIO.readFromCSV("/Users/davidsavage/Desktop/mid_from_to_date_len.txt");
-		UniqueFactory<Node> factory;
-
-		Transaction tx = graphDB.beginTx();
+	public static String getInputFromPrompt(BufferedReader in) {
+		System.out.print("> ");
 		try {
-			//Create a factory object that stores indices in an index set named employees
-			factory = new UniqueFactory.UniqueNodeFactory(graphDB, "employees") {
-				protected void initialize(Node created, Map<String, Object> properties) {
-					created.setProperty("id", properties.get("id"));
-				}
-			};
+			return in.readLine();
 		}
-		finally {
-			tx.finish();
-		}
-
-		for(EnronEmail email: entries) {
-			addEmail(graphDB, factory, email);
-		}
-	}
-
-
-	public static void addEmail(GraphDatabaseService graphDB, UniqueFactory<Node> factory, EnronEmail email) {
-		Node fromEmp, toEmp;
-		Relationship rel;
-
-		Transaction tx = graphDB.beginTx();
-		try {
-			//Create the from and to nodes if they don't already exist
-			fromEmp = factory.getOrCreate("id", email.to);
-			toEmp = factory.getOrCreate("id", email.from);
-
-			//Create a relationship between the nodes representing the email
-			rel = fromEmp.createRelationshipTo(toEmp, RelTypes.EMAILED);
-			rel.setProperty("date", email.sendDate);
-			rel.setProperty("time", email.sendTime);
-			rel.setProperty("length", email.length);
-
-			tx.success();
-		}
-		finally {
-			tx.finish();
+		catch(IOException e) {
+			e.printStackTrace();
+			return "";
 		}
 	}
 }
